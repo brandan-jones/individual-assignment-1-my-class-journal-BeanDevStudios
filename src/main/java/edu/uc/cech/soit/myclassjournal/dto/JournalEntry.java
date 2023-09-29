ffa4c0964aa451fc72ff9783507df9475d5724a6
Kelli Porter <kelli.porter1998@gmail.com>
2023-09-29 13:22:00 -0400

    All Tests Pass

--- src/main/java/edu/uc/cech/soit/myclassjournal/dto/JournalEntry.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/dto/JournalEntry.java
@@ -1,21 +1,15 @@
 package edu.uc.cech.soit.myclassjournal.dto;
 
-import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Setter
 @Getter
-public @Data
-class JournalEntry {
+public class JournalEntry {
 
     private String notes;
     private String date;
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 
 }
--- src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
@@ -1,22 +1,28 @@
 package edu.uc.cech.soit.myclassjournal.service;
 
 import edu.uc.cech.soit.myclassjournal.dto.JournalEntry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public class JournalServiceStub implements IJournalService{
+public class JournalService implements IJournalService{
+
+    ArrayList<JournalEntry> allEntries = new ArrayList<>();
 
 
     @Override
     public void save(JournalEntry journalEntry) {
+        allEntries.add(journalEntry);
 
     }
 
     @Override
     public List<JournalEntry> fetchAll() {
-        return null;
+
+        return allEntries;
     }
 }
